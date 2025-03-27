a8bae173323550c47635885babdcd99290ed33fd
Bobbaru <bobbaru_krueger@yahoo.com>
2025-03-27 15:47:20 +0200

    designs: dofristan hero header

---
+++ brand-factory/designs/dofristan/src/components/Navigation.tsx
@@ -0,0 +1,87 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { useMediaQuery } from "usehooks-ts";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  navItems: {
+    label: string;
+    href: string;
+  }[];
+}
+
+export const Navigation = ({ navItems }: Props) => {
+  const matches = useMediaQuery("(min-width: 768px)");
+
+  return (
+    <nav>
+      {!matches ? (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              className="bg-transparent p-1 [&>svg]:size-6!"
+              size={"icon"}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeWidth="1.5"
+                  d="M20 7H4m16 5H4m16 5H4"
+                />
+              </svg>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Menu</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            {navItems.map((item) => (
+              <DropdownMenuItem key={item.href} asChild>
+                <a href={item.href} className="cursor-pointer">
+                  {item.label}
+                </a>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <ol
+          className={cn(
+            "font-rubik flex gap-4 text-xs text-[oklch(from_#FFFFFF_l_c_h_/_0.5)]",
+          )}
+        >
+          {navItems.map((item, index) => {
+            const isLast = index === navItems.length - 1;
+
+            return (
+              <li key={item.href}>
+                <a
+                  href={item.href}
+                  className={cn(
+                    `hover:text-white`,
+                    `${isLast ? "font-medium text-white" : "text-[oklch(from_#FFFFFF_l_c_h_/_0.5)]"}`,
+                  )}
+                >
+                  {item.label}
+                </a>
+              </li>
+            );
+          })}
+        </ol>
+      )}
+    </nav>
+  );
+};
