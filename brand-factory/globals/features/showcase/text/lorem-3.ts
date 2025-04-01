f87736d94305d2bca31b87ba045be4f9dd36ad77
Bobbaru <bobbaru_krueger@yahoo.com>
2025-04-01 20:49:20 +0300

    globals: showcase feature small update

--- brand-factory/globals/features/showcase/text/lorem-3.ts
+++ brand-factory/globals/features/showcase/text/lorem-3.ts
@@ -3,40 +3,40 @@ export const lorem3 = (spanClasses?: string, brandName?: string): string[] => {
     return [
       `Lorem ipsum <span class="${spanClasses}">${
         brandName || "dolor"
-      }</span>.`,
+      }</span>`,
       `<span class="${spanClasses}">${
         brandName || "Vestibulum"
-      }</span> ante ipsum.`,
+      }</span> ante ipsum`,
       `Phasellus <span class="${spanClasses}">${
         brandName || "accumsan"
-      }</span> est.`,
+      }</span> est`,
       `Curabitur ullamcorper <span class="${spanClasses}">${
         brandName || "nisi"
-      }</span>.`,
-      `<span class="${spanClasses}">${brandName || "Nam"}</span> eget dui.`,
+      }</span>`,
+      `<span class="${spanClasses}">${brandName || "Nam"}</span> eget dui`,
       `Etiam <span class="${spanClasses}">${
         brandName || "rhoncus"
-      }</span> faucibus.`,
+      }</span> faucibus`,
       `Donec vitae <span class="${spanClasses}">${
         brandName || "sapien"
-      }</span>.`,
-      `<span class="${spanClasses}">${brandName || "Nullam"}</span> quis ante.`,
-      `Duis <span class="${spanClasses}">${brandName || "arcu"}</span> tortor.`,
+      }</span>`,
+      `<span class="${spanClasses}">${brandName || "Nullam"}</span> quis ante`,
+      `Duis <span class="${spanClasses}">${brandName || "arcu"}</span> tortor`,
       `Integer ante <span class="${spanClasses}">${
         brandName || "arcu"
-      }</span>.`,
+      }</span>`,
     ];
 
   return [
-    `Lorem ipsum dolor.`,
-    `Vestibulum ante ipsum.`,
-    `Phasellus accumsan est.`,
-    `Curabitur ullamcorper nisi.`,
-    `Nam eget dui.`,
-    `Etiam rhoncus faucibus.`,
-    `Donec vitae sapien.`,
-    `Nullam quis ante.`,
-    `Duis arcu tortor.`,
-    `Integer ante arcu.`,
+    `Lorem ipsum dolor`,
+    `Vestibulum ante ipsum`,
+    `Phasellus accumsan est`,
+    `Curabitur ullamcorper nisi`,
+    `Nam eget dui`,
+    `Etiam rhoncus faucibus`,
+    `Donec vitae sapien`,
+    `Nullam quis ante`,
+    `Duis arcu tortor`,
+    `Integer ante arcu`,
   ];
 };
