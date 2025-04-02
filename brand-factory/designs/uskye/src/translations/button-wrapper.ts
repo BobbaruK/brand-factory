5788004148761a33c30798d7127e55c1d2115e2c
Bobbaru <bobbaru_krueger@yahoo.com>
2025-04-02 17:13:43 +0300

    designs: uskye 2nd section

---
+++ brand-factory/designs/uskye/src/translations/button-wrapper.ts
@@ -0,0 +1,37 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export const buttonWrapperLabel: TranslationsType = {
+  en: "Start your trading now",
+  it: "",
+  tr: "",
+  ro: "",
+  ar: "",
+  de: "",
+  es: "",
+  sv: "",
+  pt: "",
+  fi: "",
+  pl: `Zacznij handlować teraz`,
+  hu: "",
+  th: "",
+  ms: "",
+  vi: "Bắt đầu giao dịch ngay",
+};
+
+export const buttonWrapperDisclaimer: TranslationsType = {
+  en: "*Terms and conditions apply. Trading is risky.",
+  it: "",
+  tr: "",
+  ro: "",
+  ar: "",
+  de: "",
+  es: "",
+  sv: "",
+  pt: "",
+  fi: "",
+  pl: `Handel jest ryzykowny. Możesz stracić zainwestowany kapitał.`,
+  hu: "",
+  th: "",
+  ms: "",
+  vi: "*Áp dụng điều khoản và điều kiện. Viẹc giao dịch tiềm ẩn rủi ro.",
+};
