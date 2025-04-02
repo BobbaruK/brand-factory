f88a075839d022d61251b57d38a122c53c8723d1
Bobbaru <bobbaru_krueger@yahoo.com>
2025-04-02 16:49:48 +0300

    designs: uskye 1st section

---
+++ brand-factory/designs/uskye/src/types/bullet.ts
@@ -0,0 +1,7 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export interface Bullet {
+  title: TranslationsType;
+  para?: TranslationsType;
+  svgIcon?: string;
+}
