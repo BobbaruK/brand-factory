ab9dd28c2037099e6cd18d288a546a81c6730ccf
Bobbaru <bobbaru_krueger@yahoo.com>
2025-04-08 14:27:03 +0300

    globals: button disclaimer component ar lang updated

--- brand-factory/globals/components/button-disclaimer/index.tsx
+++ brand-factory/globals/components/button-disclaimer/index.tsx
@@ -13,7 +13,7 @@ export const ButtonDisclaimer = ({ children, lang, ...restProps }: Props) => {
     it: `Il trading di CFD è rischioso. Potresti perdere tutto il capitale investito.`,
     tr: ``,
     ro: `Tranzacționarea CFD-urilor este riscantă. Este posibil să pierdeți tot capitalul investit.`,
-    ar: ``,
+    ar: `تداول العقود مقابل الفروقات أمر محفوف بالمخاطر. قد تفقد كل رأس المال المستثمر.`,
     de: ``,
     es: `Operar con CFDs es arriesgado. Puede perder todo el capital invertido.`,
     sv: `Trading med CFD:er är mycket riskfyllt. Du kan förlora allt investerat kapital.`,
