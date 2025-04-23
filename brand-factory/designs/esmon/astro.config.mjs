06bb6ebad547b9baaff39d35eb1809381298d01e
Bobbaru <bobbaru_krueger@yahoo.com>
2025-04-23 17:14:40 +0300

    designs: esmon update and deploy

--- brand-factory/designs/esmon/astro.config.mjs
+++ brand-factory/designs/esmon/astro.config.mjs
@@ -6,8 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/registration/v4/A/ar/",
+  // base: "/registration/v4/A/ar/",
   // base: "/registration/v4/B/ar/",
+  base: "/registration/v5/es/",
+  // base: "/registration/v3/es/",
   trailingSlash: "ignore",
   integrations: [react()],
 
