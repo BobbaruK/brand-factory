fd667dbca804f1160946b451521a6c52bb755985
Bobbaru <bobbaru_krueger@yahoo.com>
2025-04-24 16:15:01 +0300

    designs: gespeiton hero header

---
+++ brand-factory/designs/gespeiton/src/translations/form-button.ts
@@ -0,0 +1,19 @@
+import type { TranslationsType } from "../../../../globals/types/Translations";
+
+export const formButtonTranslation: TranslationsType = {
+  en: `Join our network`,
+  it: "",
+  tr: "",
+  ro: "",
+  ar: "",
+  de: "",
+  es: ``,
+  sv: "",
+  pt: "",
+  fi: "",
+  pl: "",
+  hu: "",
+  th: "",
+  ms: "",
+  vi: "",
+};
