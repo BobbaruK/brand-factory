e741150312d7ffbbbdfa2cdb73492bbdeffe1ad4
Bobbaru <bobbaru_krueger@yahoo.com>
2025-05-12 16:04:56 +0300

    designs: xobroaye update and deploy

--- brand-factory/designs/xobroaye/astro.config.mjs
+++ brand-factory/designs/xobroaye/astro.config.mjs
@@ -6,9 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/registration/v4/ar/",
+  // base: "/registration/v4/ar/",
   // base: "/registration/v4/A/ar/",
   // base: "/registration/v4/B/ar/",
+  base: "/lp/registration/v4/ar/",
   trailingSlash: "ignore",
   integrations: [react()],
 
