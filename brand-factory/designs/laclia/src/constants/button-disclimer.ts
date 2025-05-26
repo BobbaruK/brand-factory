adb147df4a2e09b4e08d410a9bb0a2f7d1ec35ae
Bobbaru <bobbaru_krueger@yahoo.com>
2025-05-26 15:36:27 +0300

    designs: laclia 4th section and small updates

---
+++ brand-factory/designs/laclia/src/constants/button-disclimer.ts
@@ -0,0 +1,19 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export const buttonDisclaimer: TranslationsType = {
+  en: `Trading is risky.`,
+  it: ``,
+  tr: ``,
+  ro: ``,
+  ar: ``,
+  de: ``,
+  es: ``,
+  sv: ``,
+  pt: ``,
+  fi: ``,
+  pl: ``,
+  hu: ``,
+  th: ``,
+  ms: ``,
+  vi: ``,
+};
