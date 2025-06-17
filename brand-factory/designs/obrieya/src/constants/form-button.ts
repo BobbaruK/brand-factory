2f4744d347a69bfb80e93adf22d04d53d6123790
Bobbaru <bobbaru_krueger@yahoo.com>
2025-06-17 14:55:09 +0300

    designs: obrieya hero header

---
+++ brand-factory/designs/obrieya/src/constants/form-button.ts
@@ -0,0 +1,19 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export const FORM_BUTTON: TranslationsType = {
+  en: `Register & Get Signals`,
+  it: ``,
+  tr: ``,
+  ro: ``,
+  ar: ``,
+  de: ``,
+  es: `Regístrate y recibe señales`,
+  sv: ``,
+  pt: ``,
+  fi: ``,
+  pl: ``,
+  hu: ``,
+  th: ``,
+  ms: ``,
+  vi: ``,
+};
