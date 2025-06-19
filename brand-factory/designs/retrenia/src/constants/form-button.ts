99b91cf1b52432c0676cb976f48580efaa8b22cd
Bobbaru <bobbaru_krueger@yahoo.com>
2025-06-19 15:17:57 +0300

    designs: retrenia hero header

---
+++ brand-factory/designs/retrenia/src/constants/form-button.ts
@@ -0,0 +1,19 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export const FORM_BUTTON: TranslationsType = {
+  en: `Get access`,
+  it: ``,
+  tr: ``,
+  ro: ``,
+  ar: ``,
+  de: ``,
+  es: ``,
+  sv: ``,
+  pt: ``,
+  fi: ``,
+  pl: ``,
+  hu: ``,
+  th: ``,
+  ms: ``,
+  vi: ``,
+};
