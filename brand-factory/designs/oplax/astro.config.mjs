1238214468c1d1a96cb54f87db011bb42639c8ee
Bobbaru <bobbaru_krueger@yahoo.com>
2025-06-26 16:38:00 +0300

    designs: oplax update and deploy

--- brand-factory/designs/oplax/astro.config.mjs
+++ brand-factory/designs/oplax/astro.config.mjs
@@ -6,7 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/lp/trading-signals/es/",
+  // base: "/lp/trading-signals/es/",
+  base: "/lp/trading-signals/v2/es/",
   // base: "/lp/trading-signals-blue/es/",
   // base: "/lp/trading-signals-blue/en/",
   trailingSlash: "ignore",
