1df74245a09c3a555176246af180cc3e3b2e25ed
Bobbaru <bobbaru_krueger@yahoo.com>
2025-06-27 14:56:58 +0300

    designs: obrieya update and deploy

--- brand-factory/designs/obrieya/astro.config.mjs
+++ brand-factory/designs/obrieya/astro.config.mjs
@@ -6,8 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/lp/ai-commodities/en/",
-  // base: "/lp/ai-commodities/es/",
+  // base: "/lp/ai-commodities/en/",
+  base: "/lp/ai-commodities/es/",
   trailingSlash: "ignore",
   integrations: [react()],
 
