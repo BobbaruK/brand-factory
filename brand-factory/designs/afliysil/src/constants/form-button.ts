ee77364665e76991834ae3c91e3a9c52a7bcd876
Bobbaru <bobbaru_krueger@yahoo.com>
2025-06-27 12:15:42 +0300

    designs: afliysil hero header - partial

---
+++ brand-factory/designs/afliysil/src/constants/form-button.ts
@@ -0,0 +1,19 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export const FORM_BUTTON: TranslationsType = {
+  en: `Get access`,
+  it: `Ottieni accesso`,
+  tr: ``,
+  ro: ``,
+  ar: ``,
+  de: ``,
+  es: ``,
+  sv: `Få tillgång`,
+  pt: ``,
+  fi: ``,
+  pl: `Uzyskaj dostęp`,
+  hu: ``,
+  th: ``,
+  ms: ``,
+  vi: ``,
+};
