88f997094c2f9e0103db53078e454104e4868e1b
Bobbaru <bobbaru_krueger@yahoo.com>
2025-06-27 14:20:38 +0300

    deigns: agluistein new text and design version

--- brand-factory/designs/agluistein/src/components/ui/button.tsx
+++ brand-factory/designs/agluistein/src/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        white: "bg-white text-white-foreground shadow-xs hover:bg-white/90",
       },
       size: {
         default: "h-auto px-4 py-2 has-[>svg]:px-3",
