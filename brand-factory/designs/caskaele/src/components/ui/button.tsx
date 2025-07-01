e25230cf451a73fd639f5af37bf438a92b6e3710
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-01 15:44:18 +0300

    designs: caskaele hero header

--- brand-factory/designs/caskaele/src/components/ui/button.tsx
+++ brand-factory/designs/caskaele/src/components/ui/button.tsx
@@ -25,7 +25,7 @@ const buttonVariants = cva(
           "bg-accent text-accent-foreground shadow-xs hover:bg-accent/90",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        default: "h-auto px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
---
+++ brand-factory/designs/caskaele/src/constants/custom-disclaimer.ts
@@ -0,0 +1,19 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export const CUSTOM_DISCLAIMER: TranslationsType = {
+  en: `Trading is risky. <a href="#" target="_blank">Terms and conditions</a> apply.`,
+  it: ``,
+  tr: ``,
+  ro: ``,
+  ar: ``,
+  de: ``,
+  es: `Operar implica riesgos. Aplican <a href="#" target="_blank">términos y condiciones</a>.`,
+  sv: ``,
+  pt: ``,
+  fi: ``,
+  pl: ``,
+  hu: ``,
+  th: ``,
+  ms: ``,
+  vi: ``,
+};
