73472142fe4f1586a2336e2ecfe335b05673d339
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-02 17:58:23 +0300

    designs: update and deploy

--- brand-factory/designs/butreuna/src/constants/custom-disclaimer.ts
+++ brand-factory/designs/butreuna/src/constants/custom-disclaimer.ts
@@ -1,13 +1,13 @@
 import type { TranslationsType } from "../../../../globals/types/translations";
 
 export const CUSTOM_DISCLAIMER: TranslationsType = {
-  en: `Trading is risky. <a target="_blank" href="https://fxoroglobal.com/cashback/" target="_blank">Terms and conditions</a> apply.`,
+  en: `Trading is risky.<br /> <a target="_blank" href="https://fxoroglobal.com/cashback/" target="_blank">Terms and conditions</a> apply.`,
   it: ``,
   tr: ``,
   ro: ``,
-  ar: `التداول محفوف بالمخاطر. <a target="_blank" href="https://fxoroglobal.com/ar/cashback/">تُطبّق الشروط والأحكا</a>م.`,
+  ar: `التداول محفوف بالمخاطر. <br /><a target="_blank" href="https://fxoroglobal.com/ar/cashback/">تُطبّق الشروط والأحكا</a>م.`,
   de: ``,
-  es: `Operar implica riesgos. Aplican <a target="_blank" href="https://fxoroglobal.com/es/cashback/" target="_blank">términos y condiciones</a>.`,
+  es: `Operar implica riesgos.<br /> Aplican <a target="_blank" href="https://fxoroglobal.com/es/cashback/" target="_blank">términos y condiciones</a>.`,
   sv: ``,
   pt: ``,
   fi: ``,
