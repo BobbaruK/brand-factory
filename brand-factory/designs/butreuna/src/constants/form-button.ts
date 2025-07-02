9be3ff9179c0697f642187980b3a89a851488d1b
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-02 16:55:55 +0300

    designs: butreuna handle modal form and small refactors

---
+++ brand-factory/designs/butreuna/src/constants/form-button.ts
@@ -0,0 +1,19 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export const FORM_BUTTON: TranslationsType = {
+  en: `Claim now`,
+  it: ``,
+  tr: ``,
+  ro: ``,
+  ar: `احصل عليه الآن`,
+  de: ``,
+  es: `Reclámalo ahora`,
+  sv: ``,
+  pt: ``,
+  fi: ``,
+  pl: ``,
+  hu: ``,
+  th: ``,
+  ms: ``,
+  vi: ``,
+};
