82c8152c3ae9afd8a3cddc0b5a939679f6ec8c50
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-02 17:52:42 +0300

    designs: small updates and test deploy

--- brand-factory/designs/butreuna/astro.config.mjs
+++ brand-factory/designs/butreuna/astro.config.mjs
@@ -1,16 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/lp/LP_NAME/LP_LANG/",
-	trailingSlash: "ignore",
-	integrations: [react()],
+  base: "/deposit-bonus/en/",
+  // base: "/deposit-bonus/es/",
+  // base: "/deposit-bonus/ar/",
+  trailingSlash: "ignore",
+  integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
