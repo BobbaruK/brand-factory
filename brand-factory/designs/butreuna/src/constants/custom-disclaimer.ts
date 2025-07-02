b4a4856ad4c3618f3d69f21b317d4d90d0be9530
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-02 15:50:26 +0300

    designs: butreuna hero header and para lead lh updated

---
+++ brand-factory/designs/butreuna/src/constants/custom-disclaimer.ts
@@ -0,0 +1,19 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export const CUSTOM_DISCLAIMER: TranslationsType = {
+  en: `Trading is risky. <a target="_blank" href="https://fxoroglobal.com/cashback/" target="_blank">Terms and conditions</a> apply.`,
+  it: ``,
+  tr: ``,
+  ro: ``,
+  ar: `التداول محفوف بالمخاطر. <a target="_blank" href="https://fxoroglobal.com/ar/cashback/">تُطبّق الشروط والأحكا</a>م.`,
+  de: ``,
+  es: `Operar implica riesgos. Aplican <a target="_blank" href="https://fxoroglobal.com/es/cashback/" target="_blank">términos y condiciones</a>.`,
+  sv: ``,
+  pt: ``,
+  fi: ``,
+  pl: ``,
+  hu: ``,
+  th: ``,
+  ms: ``,
+  vi: ``,
+};
