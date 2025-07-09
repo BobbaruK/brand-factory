1209fc4e2508609b02bfc8d9bb0b72451432ef41
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-09 15:41:57 +0300

    designs: cuwheice 2nd section

---
+++ brand-factory/designs/cuwheice/src/constants/icons/index.ts
@@ -0,0 +1,8 @@
+export { ICON1 } from "./icon1";
+export { ICON2 } from "./icon2";
+export { ICON3 } from "./icon3";
+export { ICON4 } from "./icon4";
+export { ICON5 } from "./icon5";
+export { ICON6 } from "./icon6";
+export { ICON7 } from "./icon7";
+export { ICON8 } from "./icon8";
