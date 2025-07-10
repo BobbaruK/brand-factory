5c2311960ec5ecd77aa4bb0718cce40e160de899
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-10 16:18:42 +0300

    designs: votreace small updates

---
+++ brand-factory/designs/votreace/src/constants/form-button-label.ts
@@ -0,0 +1,19 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export const FORM_BUTTON_LABEL: TranslationsType = {
+  en: `Get index signals now`,
+  it: ``,
+  tr: ``,
+  ro: ``,
+  ar: ``,
+  de: ``,
+  es: ``,
+  sv: ``,
+  pt: ``,
+  fi: ``,
+  pl: ``,
+  hu: ``,
+  th: ``,
+  ms: ``,
+  vi: ``,
+};
