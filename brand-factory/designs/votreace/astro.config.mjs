0c0d8950adb121dd9d3560643636855319d63aed
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-10 16:49:18 +0300

    designs: votreace update and deploy

--- brand-factory/designs/votreace/astro.config.mjs
+++ brand-factory/designs/votreace/astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/lp/LP_NAME/LP_LANG/",
-	trailingSlash: "ignore",
-	integrations: [react()],
+  // base: "/lp/indices-and-signals/en/",
+  base: "/lp/indices-and-signals/es/",
+  trailingSlash: "ignore",
+  integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
