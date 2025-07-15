ee1c4feb5561cb5ca176c5d9ee0cd9a5d3eb134a
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-15 14:13:07 +0300

    designs: butreuna update and deploy

--- brand-factory/designs/butreuna/astro.config.mjs
+++ brand-factory/designs/butreuna/astro.config.mjs
@@ -7,8 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   // base: "/deposit-bonus/en/",
-  // base: "/deposit-bonus/es/",
-  base: "/deposit-bonus/ar/",
+  base: "/deposit-bonus/es/",
+  // base: "/deposit-bonus/ar/",
   trailingSlash: "ignore",
   integrations: [react()],
 
