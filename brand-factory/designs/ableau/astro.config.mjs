ae308cc305205d98b0c6a8f5e5ea84ac1d11bc70
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-16 15:42:38 +0300

    designs: ableau cysec, update and deploy

--- brand-factory/designs/ableau/astro.config.mjs
+++ brand-factory/designs/ableau/astro.config.mjs
@@ -6,8 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/lp/corsitrading/v1/es/",
+  // base: "/lp/corsitrading/v1/es/",
   // base: "/lp/corsitrading/v1/it/",
+  base: "/corsitrading/v1_eu/pl/",
   trailingSlash: "ignore",
   integrations: [react()],
 
