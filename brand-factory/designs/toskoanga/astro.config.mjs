4a651e1aca95931d53594f8a3e2bbeece72201f3
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-16 15:43:08 +0300

    designs: toskoanga cysec, update and deploy

--- brand-factory/designs/toskoanga/astro.config.mjs
+++ brand-factory/designs/toskoanga/astro.config.mjs
@@ -6,12 +6,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  // base: "/bitcoin25/sv/",
+  base: "/bitcoin25/sv/",
   // base: "/bitcoin25/it/",
   // base: "/bitcoin25/en/",
   // base: "/bitcoin25/es/",
   // base: "/bitcoin25/ar/",
-  base: "/bitcoin25/pl/",
+  // base: "/bitcoin25/pl/",
   // base: "/lp/bitcoin25/en/",
   // base: "/lp/bitcoin25/es/",
   // base: "/lp/bitcoin25/ar/",
