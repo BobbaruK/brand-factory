d0b125ef89245256f9aa7f26c4cba52bc48db9d7
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-17 17:39:06 +0300

    designs: pocruosal handle icons and small updates

---
+++ brand-factory/designs/pocruosal/src/constants/icons/index.ts
@@ -0,0 +1,7 @@
+export { ICON1 } from "./icon1";
+export { ICON2 } from "./icon2";
+export { ICON3 } from "./icon3";
+export { ICON4 } from "./icon4";
+export { ICON5 } from "./icon5";
+export { ICON6 } from "./icon6";
+export { ICON7 } from "./icon7";
