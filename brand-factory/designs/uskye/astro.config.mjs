0fe1f55f4889a7f279baf84739e1afe263553d11
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-17 14:29:08 +0300

    designs: uskye cysec, update and deploy

--- brand-factory/designs/uskye/astro.config.mjs
+++ brand-factory/designs/uskye/astro.config.mjs
@@ -6,8 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/cfd-25-bonus/es/",
-  // base: "/cfd-bonus/en/",
+  // base: "/cfd-25-bonus/es/",
+  base: "/cfd-bonus/en/",
+  // base: "/cfds-stocks/pl/",
   trailingSlash: "ignore",
   integrations: [react()],
 
