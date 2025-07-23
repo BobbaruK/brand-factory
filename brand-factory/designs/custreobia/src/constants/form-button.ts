34c4b02a90abea8882c663aa03e7cd007c90e654
Bobbaru <bobbaru_krueger@yahoo.com>
2025-07-23 13:56:33 +0300

    designs: cutreobia handle modal form

---
+++ brand-factory/designs/custreobia/src/constants/form-button.ts
@@ -0,0 +1,19 @@
+import type { TranslationsType } from "../../../../globals/types/translations";
+
+export const FORM_BUTTON: TranslationsType = {
+  en: `Sign up today`,
+  it: ``,
+  tr: ``,
+  ro: ``,
+  ar: `سجل اليوم`,
+  de: ``,
+  es: `Regístrate hoy`,
+  sv: ``,
+  pt: ``,
+  fi: ``,
+  pl: ``,
+  hu: ``,
+  th: ``,
+  ms: ``,
+  vi: ``,
+};
