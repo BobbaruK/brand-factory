e4835e02080ea7f734e0ec9029c77135086e2793
Nicoleta Mirea <nicoleta8905@gmail.com>
2025-08-07 13:33:11 +0300

    designs: Puwhus changes

--- brand-factory/designs/puwhus/astro.config.mjs
+++ brand-factory/designs/puwhus/astro.config.mjs
@@ -6,9 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/livetradingsignals/v2/en/",
+  // base: "/livetradingsignals/v2/en/",
   // base: "/livetradingsignals/v2/es/",
-  // base: "/livetradingsignals/v2/ar/",
+  base: "/livetradingsignals/v2/ar/",
   trailingSlash: "ignore",
   integrations: [react()],
 
