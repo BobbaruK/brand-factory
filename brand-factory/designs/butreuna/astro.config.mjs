2330e05176ad0f71a809ef66ed3d33abfc7e3160
Bobbaru-K <bobbaru311@gmail.com>
2025-08-22 18:01:22 +0300

    designs: butreuna update before deploy

--- brand-factory/designs/butreuna/astro.config.mjs
+++ brand-factory/designs/butreuna/astro.config.mjs
@@ -6,8 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  // base: "/deposit-bonus/en/",
-  base: "/deposit-bonus/es/",
+  base: "/deposit-bonus/en/",
+  // base: "/deposit-bonus/es/",
   // base: "/deposit-bonus/ar/",
   trailingSlash: "ignore",
   integrations: [react()],
