81fbd56a264d60188c6c06481761196acf75eea1
Bobbaru-K <bobbaru311@gmail.com>
2025-08-26 15:48:56 +0300

    deploy: radrieso - update settings

--- brand-factory/designs/radrieso/astro.config.mjs
+++ brand-factory/designs/radrieso/astro.config.mjs
@@ -6,11 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  // base: "/lp/trading_signals/ar/",
+  base: "/lp/trading_signals/ar/",
   // base: "/lp/trading_signals/en/",
   // base: "/lp/trading_signals/es/",
   // base: "/trading_signals/ar/",
-  base: "/trading_signals/ar/",
+  // base: "/trading_signals/ar/",
   trailingSlash: "ignore",
   integrations: [react()],
 
