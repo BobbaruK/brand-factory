2f81eb6bbe9e3f094ca3a4eb8e0e7bff756fd3ba
Bobbaru-K <bobbaru311@gmail.com>
2025-08-28 14:54:44 +0300

    deploy: puskeile - update settings

--- brand-factory/designs/puskeile/astro.config.mjs
+++ brand-factory/designs/puskeile/astro.config.mjs
@@ -6,9 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  // base: "/lion-gold/v2/ar/",
-  // base: "/lp/lion-gold/v2/ar/",
   base: "/lion-gold/v2/ar/",
+  // base: "/lp/lion-gold/v2/ar/",
   trailingSlash: "ignore",
   integrations: [react()],
 
