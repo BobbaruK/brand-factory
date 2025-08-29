34b089228b9c5ab00a8a5d1ea5d52e839d8b1d6f
Nicoleta Mirea <nicoleta8905@gmail.com>
2025-08-29 11:40:26 +0300

    deploy: xobroaye - update settings

--- brand-factory/designs/xobroaye/astro.config.mjs
+++ brand-factory/designs/xobroaye/astro.config.mjs
@@ -6,11 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  // base: "/registration/v4/ar/",
+  base: "/registration/v4/ar/",
   // base: "/registration/v4/A/ar/",
   // base: "/registration/v4/B/ar/",
   // base: "/lp/registration/v4/ar/",
-  base: "/lp/registration/v4/A/ar/",
+  // base: "/lp/registration/v4/A/ar/",
   trailingSlash: "ignore",
   integrations: [react()],
 
